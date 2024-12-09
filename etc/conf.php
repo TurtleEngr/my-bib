345dcfa37842559fa31a0db1fc0545a3b020cf0d
TurtleEngr <turtle.engr@gmail.com>
2024-12-08 17:28:02 -0800

    Updated

---
+++ etc/conf.php
@@ -0,0 +1,95 @@
+<?php
+# Generated with gen-conf-php.sh
+
+global $cgDebug;
+$cgDebug=$_ENV["cgDebug"];
+
+global $cgNoExec;
+$cgNoExec=$_ENV["cgNoExec"];
+
+global $cgVerbose;
+$cgVerbose=$_ENV["cgVerbose"];
+
+global $cgDirBackup;
+$cgDirBackup=$_ENV["cgDirBackup"];
+
+global $cgDirConf;
+$cgDirConf=$_ENV["cgDirConf"];
+
+global $cgDirEtc;
+$cgDirEtc=$_ENV["cgDirEtc"];
+
+global $cgDirLibreofficeConf;
+$cgDirLibreofficeConf=$_ENV["cgDirLibreofficeConf"];
+
+global $cgDirStatus;
+$cgDirStatus=$_ENV["cgDirStatus"];
+
+global $cgDirTmp;
+$cgDirTmp=$_ENV["cgDirTmp"];
+
+global $cgBackupNum;
+$cgBackupNum=$_ENV["cgBackupNum"];
+
+global $cgDbBib;
+$cgDbBib=$_ENV["cgDbBib"];
+
+global $cgDbHost;
+$cgDbHost=$_ENV["cgDbHost"];
+
+global $cgDbHostRemote;
+$cgDbHostRemote=$_ENV["cgDbHostRemote"];
+
+global $cgDbLib;
+$cgDbLib=$_ENV["cgDbLib"];
+
+global $cgDbLo;
+$cgDbLo=$_ENV["cgDbLo"];
+
+global $cgDbName;
+$cgDbName=$_ENV["cgDbName"];
+
+global $cgDbPassCache;
+$cgDbPassCache=$_ENV["cgDbPassCache"];
+
+global $cgDbPassHint;
+$cgDbPassHint=$_ENV["cgDbPassHint"];
+
+global $cgDbPortLocal;
+$cgDbPortLocal=$_ENV["cgDbPortLocal"];
+
+global $cgDbPortRemote;
+$cgDbPortRemote=$_ENV["cgDbPortRemote"];
+
+global $cgDbUser;
+$cgDbUser=$_ENV["cgDbUser"];
+
+global $cgUseRemote;
+$cgUseRemote=$_ENV["cgUseRemote"];
+
+global $cgSshUser;
+$cgSshUser=$_ENV["cgSshUser"];
+
+global $cgSshKey;
+$cgSshKey=$_ENV["cgSshKey"];
+
+global $cgUseLib;
+$cgUseLib=$_ENV["cgUseLib"];
+
+global $cgLibFile;
+$cgLibFile=$_ENV["cgLibFile"];
+
+global $cgLoFile;
+$cgLoFile=$_ENV["cgLoFile"];
+
+global $cgDocFile;
+$cgDocFile=$_ENV["cgDocFile"];
+
+global $cgBackupFile;
+$cgBackupFile=$_ENV["cgBackupFile"];
+
+global $cgBin;
+$cgBin=$_ENV["cgBin"];
+
+global $cgDirApp;
+$cgDirApp=$_ENV["cgDirApp"];
